368cfa836b74bae674f7fd6e1bbb55930e8e7a54
Toby Hammond <tobiasjhammond@gmail.com>
2023-11-30 13:43:05 -0800

    Working build with illegal use of c-strings

--- assignments/pa4/pa4.cpp
+++ assignments/pa4/pa4.cpp
@@ -13,12 +13,14 @@
 
 using namespace std;
 
+const double SELL_PRICE_OFFSET = 0.8;
+
 struct Part {
   char name[10];
   char manufacturer[11];
-  int actual_quantity;
-  int min_quantity;
-  double unit_price;
+  int actual_q;
+  int min_q;
+  double uprice;
 };
 
 int DisplayMenu();
@@ -26,14 +28,16 @@ int ReadPartInfo(Part[], ifstream &, double &);
 void DisplayInventoryReport(Part[], int, double);
 void DisplayReorderReport(Part[], int, double);
 void SellPart(Part[], int, double &);
-void OrderPart();
-int ProcessSale(Part[], int);
+void OrderPart(Part[], int, double &);
+int ProcessQuery(Part[], int);
 
 int main() {
   ifstream in("inventory.dat");
 
   Part parts[150];
   double balance;
+  // Reads Part data from inventory.dat into Parts, num_parts and balance, for
+  // use later.
   int num_parts = ReadPartInfo(parts, in, balance);
 
   while (true) {
@@ -48,6 +52,7 @@ int main() {
       SellPart(parts, num_parts, balance);
       break;
     case 4:
+      OrderPart(parts, num_parts, balance);
       break;
     default:
       exit(0);
@@ -58,16 +63,21 @@ int main() {
 
 int DisplayMenu() {
   cout << "==========================================" << endl;
-  cout << "\tMini Electronics Inventory Management" << endl;
-  cout << "\t\t1 - Inventory report" << endl;
-  cout << "\t\t2 - Reorder report" << endl;
-  cout << "\t\t3 - Sell parts" << endl;
-  cout << "\t\t4 - Order parts" << endl;
-  cout << "\t\t5 - Quit" << endl;
+  cout << "  Mini Electronics Inventory Management" << endl;
+  cout << "    1 - Inventory report" << endl;
+  cout << "    2 - Reorder report" << endl;
+  cout << "    3 - Sell parts" << endl;
+  cout << "    4 - Order parts" << endl;
+  cout << "    5 - Quit" << endl;
   cout << "==========================================" << endl;
   cout << "Your choice [1-5]: ";
 
   int input;
+  // Handy input validation control flow used ubiquitously througout the
+  // program. Firstly, checks if cin can read into its variable with no errors;
+  // if it can't give the user an error message and try again. cin.clear()
+  // clears all state flags which might have been tripped during the function,
+  // and fflush(stdin) flushes the buffer.
   while (true) {
     if (cin >> input && input > 0 && input < 6) {
       return input;
@@ -81,16 +91,19 @@ int DisplayMenu() {
 
 int ReadPartInfo(Part array[], ifstream &file_stream, double &bank_balance) {
   int num_parts;
+  // First two tokens of inventory.dat are the number of parts and the balance;
+  // quickly load those in before doing anything else.
   file_stream >> num_parts >> bank_balance;
   for (int i = 0; i < num_parts; i++) {
+    // Per line, load data into their respective fields.
     file_stream >> array[i].name >> array[i].manufacturer >>
-        array[i].actual_quantity >> array[i].min_quantity >>
-        array[i].unit_price;
+        array[i].actual_q >> array[i].min_q >> array[i].uprice;
   }
   return num_parts;
 }
 
 void DisplayInventoryReport(Part array[], int num_parts, double bank_balance) {
+  // I love setw
   cout << "--------------------------------------- Inventory Report "
           "--------------------------------------"
        << endl;
@@ -100,14 +113,15 @@ void DisplayInventoryReport(Part array[], int num_parts, double bank_balance) {
 
   int total_quantity = 0;
   double total_value = 0;
+  // Iterate through all the parts to tally some totals, as well as display the
+  // parts themselves on the screen.
   for (int i = 0; i < num_parts; i++) {
     cout << setw(20) << array[i].name << setw(20) << array[i].manufacturer
-         << setw(20) << array[i].actual_quantity << setw(20)
-         << array[i].min_quantity << setw(20)
-         << array[i].unit_price * array[i].actual_quantity << endl;
+         << setw(20) << array[i].actual_q << setw(20) << array[i].uprice
+         << setw(20) << array[i].uprice * array[i].actual_q << endl;
 
-    total_quantity += array[i].actual_quantity;
-    total_value += array[i].unit_price * array[i].actual_quantity;
+    total_quantity += array[i].actual_q;
+    total_value += array[i].uprice * array[i].actual_q;
   }
 
   cout << endl;
@@ -129,14 +143,18 @@ void DisplayReorderReport(Part array[], int num_parts, double bank_balance) {
   int to_order = 0;
   double total_cost = 0;
   for (int i = 0; i < num_parts; i++) {
-    if (array[i].actual_quantity < array[i].min_quantity) {
+    // Check to see if a given part's on-hand quantity is less than the minimum
+    // listed in inventory.dat; if so, display it in the report
+    if (array[i].actual_q < array[i].min_q) {
       cout << setw(20) << array[i].name << setw(20) << array[i].manufacturer
-           << setw(20) << array[i].actual_quantity << setw(20)
-           << array[i].min_quantity << setw(20) << array[i].unit_price << endl;
+           << setw(20) << array[i].actual_q << setw(20) << array[i].min_q
+           << setw(20) << array[i].uprice << endl;
       num_understocked++;
-      to_order += array[i].min_quantity - array[i].actual_quantity;
-      total_cost += array[i].unit_price *
-                    (array[i].min_quantity - array[i].actual_quantity) * 0.8;
+      to_order += array[i].min_q - array[i].actual_q;
+      // According to the rules, the listed "unit price" actually only regards
+      // the sell price. The buying price is 20% less.
+      total_cost += array[i].uprice * (array[i].min_q - array[i].actual_q) *
+                    SELL_PRICE_OFFSET;
     }
   }
 
@@ -148,27 +166,135 @@ void DisplayReorderReport(Part array[], int num_parts, double bank_balance) {
 }
 
 void SellPart(Part array[], int num_parts, double &balance) {
-  int part_i = ProcessSale(array, num_parts);
+  // Input validation for the desired part.
+  int part_i = ProcessQuery(array, num_parts);
   int quant;
-  cout << "Please input the quantity[0 - " << array[part_i].actual_quantity
+  // The maximum quantity of a particular part one can sell is 1 less than the
+  // on-hand quantity, since we don't want to exhaust the store's supply on any
+  // one part.
+  cout << "Please input the quantity[0 - " << array[part_i].actual_q - 1
        << "]: ";
+  // Another instance of our trusty input validation algorithm.
   while (true) {
-    if (cin >> quant && quant > 0 && quant < array[part_i].actual_quantity) {
-      array[part_i].actual_quantity -= quant;
-      balance += quant * array[part_i].unit_price;
+    if (cin >> quant && quant > 0 && quant < array[part_i].actual_q) {
+      // If desired quantity sold is within bounds, update the part data and our
+      // bank balance accordingly.
+      array[part_i].actual_q -= quant;
+      // As noted above, the selling price is just the unit price listed in
+      // inventory.dat
+      balance += quant * array[part_i].uprice;
       break;
     } else {
       cin.clear();
       fflush(stdin);
-      cout << "Please input the quantity[0 - " << array[part_i].actual_quantity
+      cout << "Please input the quantity[0 - " << array[part_i].actual_q - 1
            << "]: ";
     }
   }
 }
 
-void OrderPart(Part array[], int num_parts, double &balance) {}
+void OrderPart(Part array[], int num_parts, double &balance) {
+  // index for the most understocked part
+  int part_index;
+  Part lowest = array[0];
+  // Counter for how many understocked parts there are; if there's 0, we ask the
+  // user which one they want to order, instead of choosing for them based on
+  // necessity.
+  int ct = 0;
 
-int ProcessSale(Part array[], int num_parts) {
+  for (int i = 0; i < num_parts; i++) {
+    // if part is not understocked (the on-hand quantity is greater than the
+    // minimum quantity), skip it and move on. This greatly speeds things up,
+    // also the workaround mentioned below doesn't work if either of the
+    // operands are negative.
+    if (array[i].min_q <= array[i].actual_q) {
+      continue;
+    } else {
+      // Workaround for floating-point comparison. According to the assignment
+      // sheet, comparing a/b and c/d is the same as comparing a * d and b * c.
+      // Not sure why, but frankly it's too late in the evening to be
+      // questioning the assignment description.
+      //
+      // Anyways, this case handles when the selected Part's score is higher
+      // than the one in lowest.
+      if (((array[i].min_q - array[i].actual_q) * lowest.min_q) >
+          ((lowest.min_q - lowest.actual_q) * array[i].min_q)) {
+        lowest = array[i];
+        part_index = i;
+        ct++;
+        // Handles case when Part scores are equal; first break ties by name,
+        // then by manufacturer.
+      } else if (((array[i].min_q - array[i].actual_q) * lowest.min_q) ==
+                 ((lowest.min_q - lowest.actual_q) * array[i].min_q)) {
+        // If part names are equal, evaluate the manufacturer names for equality
+        if (!(strcmp(array[i].name, lowest.name))) {
+          // It doesn't really matter if the manufacturer names are equal, since
+          // the assignment description doesn't provide any course of action
+          // given this case.
+          if (strcmp(array[i].manufacturer, lowest.manufacturer) == -1) {
+            lowest = array[i];
+            part_index = i;
+            ct++;
+          }
+        } else {
+          if (strcmp(array[i].name, lowest.name) == -1) {
+            lowest = array[i];
+            part_index = i;
+            ct++;
+          }
+        }
+      }
+    }
+  }
+
+  // Since we have a base case at the top, if we have no understocked parts it
+  // should be pretty quick to get to this point. This case handles letting the
+  // user pick a part name and manufacturer, as well as the quantity desired.
+  if (ct == 0) {
+    part_index = ProcessQuery(array, num_parts);
+    int max_quantity = static_cast<int>(
+        balance / (array[part_index].uprice * SELL_PRICE_OFFSET));
+    cout << "Please input the quantity [0 - " << max_quantity << "]: ";
+
+    int quantity;
+    // Yet another input validation instance. Same algorithm, applied elsewhere.
+    while (true) {
+      if (cin >> quantity && quantity > 0 && quantity < max_quantity) {
+        array[part_index].actual_q += quantity;
+        balance -= array[part_index].uprice * quantity;
+        break;
+      } else {
+        cout << "Please enter a valid quantity [0 - " << max_quantity << "]: ";
+        cin.clear();
+        fflush(stdin);
+      }
+    }
+    // This case chooses the part for the user, the part in question computed by
+    // that mass of logic above.
+  } else {
+    int max_quantity = static_cast<int>(
+        balance / (array[part_index].uprice * SELL_PRICE_OFFSET));
+
+    cout << "Order Part " << array[part_index].name << " from "
+         << array[part_index].manufacturer << endl;
+    cout << "You may order quantity [0 - " << max_quantity << "]: ";
+
+    int quantity;
+    while (true) {
+      if (cin >> quantity && quantity > 0 && quantity < max_quantity) {
+        array[part_index].actual_q += quantity;
+        balance -= array[part_index].uprice * SELL_PRICE_OFFSET * quantity;
+        break;
+      } else {
+        cout << "Please enter a valid quantity [0 - " << max_quantity << "]: ";
+        cin.clear();
+        fflush(stdin);
+      }
+    }
+  }
+}
+
+int ProcessQuery(Part array[], int num_parts) {
   cout << "Input the part name: ";
   char input_name[10];
   char input_manufacturer[11];
@@ -191,6 +317,10 @@ int ProcessSale(Part array[], int num_parts) {
                   return j;
                 }
               }
+              cout << "Invalid part manufacturer!" << endl
+                   << "Input the part manufacturer: ";
+              cin.clear();
+              fflush(stdin);
             } else {
               cout << "Invalid part manufacturer!" << endl
                    << "Input the part manufacturer: ";
